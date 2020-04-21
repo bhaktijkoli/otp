a069089eaecf78a84a3e169dafab6db8da49c9d5
Bhaktij Koli <bhaktijkoli121@gmail.com>
2020-04-21 21:27:21 +0530

    Fixed Migration, Updated OTP

--- src/OTP.php
+++ src/OTP.php
@@ -7,27 +7,43 @@ use Illuminate\Support\Str;
 
 class OTP extends Model
 {
+  protected $table = "otps";
+
   public function getType()
   {
     return $this->belongsTo($this->type, 'type_id');
   }
 
-  public static function generate($model)
+  public static function generate($model, $password=null)
   {
+    $type = get_class($model);
+    $type_id = $model->getKey();
+    $otp = OTP::where('type', $type)->where('type_id', $type_id)->first();
+    if($otp) {
+      $otp->forceDelete();
+    }
     $otp = new OTP();
-    $otp->type = get_class($model);
-    $otp->type_id = $model->getKey();
-    $otp->password = "123456";
+    $otp->type = $type;
+    $otp->type_id = $type_id;
+    if($password) {
+      $otp->password = $password;
+    } else {
+      $otp->password = rand(000000,999999);
+    }
     $otp->token = Str::random(128);
     $otp->save();
-    return $otp->token;
+    return [
+      'password' => $otp->password,
+      'token' => $otp->token,
+    ];
   }
 
   public static function verify($token, $password)
   {
     $otp = OTP::where('token', $token)->where("password", $password)->first();
     if(!$otp) return null;
-    return $otp->type_id;
+    $type = "$otp->type";
+    return $type::find($otp->type_id);
   }
 
 }
